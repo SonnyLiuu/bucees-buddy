fc91dccdcedf78dd162007f31f03d500fbc4b786
SonnyLiuu <sonnyliu0213@gmail.com>
2024-09-17 21:47:13 -0400

    Updated Map Marker Feature

--- frontend/src/map/pages/MapOverview.js
+++ frontend/src/map/pages/MapOverview.js
@@ -1,21 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Map from '../../shared/components/UIElements/Map';
-import Card from '../../shared/components/UIElements/Card';
-import './MapOverview.css';
-
+import Map from "../../shared/components/UIElements/Map";
+import "./MapOverview.css";
 
 const MapOverview = (props) => {
-
-    const showMap = useState(true);
-    const {userData, Trips} = props;
-    return ( 
-      // Overview of all locations on login
-      <div className="map-overview-container" show={showMap}>
-          <Map userData={userData} Trips={Trips} latitude={32.44918332468353} longitude={-88.4046182840415} zoom={5.2} multipleMarkers={true}/>
-      </div>
-    );
-
+  const showMap = useState(true);
+  const { userData, Trips } = props;
+  return (
+    // Overview of all locations on login
+    <div className="map-overview-container" show={showMap}>
+      <Map
+        userData={userData}
+        Trips={Trips}
+        latitude={32.44918332468353}
+        longitude={-88.4046182840415}
+        zoom={5.1}
+        multipleMarkers={true}
+      />
+    </div>
+  );
 };
 
 export default MapOverview;
--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const userRoutes = require("./routes/user");
 // express app
 const app = express();
 
-
 // middleware
 app.use(express.json());
-
 
 // routes
 app.use("/api", authenticateRoutes);
@@ -27,19 +25,19 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  })
+  });
 }
 
-const port = process.env.PORT || 2500;
+const port = process.env.PORT;
 app.listen(port, console.log(`Listening on port ${port}...`));
 
-
-//This is where mongoose connects to the MongoDB 
-mongoose.connect(process.env.MONGODB_URI)
-.then(() => {
-  console.log("connected to the db and listening on port", process.env.PORT)
-})
-.catch((error) => {
-  console.log("Could not connect to database!");
-  console.log(error)
-})
+//This is where mongoose connects to the MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("connected to the db and listening on port", process.env.PORT);
+  })
+  .catch((error) => {
+    console.log("Could not connect to database!");
+    console.log(error);
+  });
